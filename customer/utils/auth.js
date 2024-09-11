469f28626e82b7b137554e809986494f5a7d0848
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-09-11 11:08:02 +0530

    Fn to sign token.

--- customer/utils/auth.js
+++ customer/utils/auth.js
@@ -1,5 +1,8 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export async function hashPassword(password) {
   return await bcrypt.hash(password, 12);
@@ -7,4 +10,8 @@ export async function hashPassword(password) {
 
 export async function verifyPassword(plainPassword, hashPassword) {
   return await bcrypt.compare(plainPassword, hashPassword);
+}
+
+export function signToken(userId, expiresIn = "24h") {
+  return jwt.sign({ userId }, process.env.JWT_KEY, { expiresIn });
 }
