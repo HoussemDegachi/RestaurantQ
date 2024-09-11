c608ea9230fb5ccf1e669a1e1a03a0c08c0ee486
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-09-11 11:06:18 +0530

    Fn to verify password.

--- customer/utils/auth.js
+++ customer/utils/auth.js
@@ -4,3 +4,7 @@ import bcrypt from "bcrypt";
 export async function hashPassword(password) {
   return await bcrypt.hash(password, 12);
 }
+
+export async function verifyPassword(plainPassword, hashPassword) {
+  return await bcrypt.compare(plainPassword, hashPassword);
+}
